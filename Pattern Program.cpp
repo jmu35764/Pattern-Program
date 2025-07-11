e5541750ae3322a65e6766e4bec14f4e911ab2a0
jm underwood <jm.underwood@hotmail.com>
2025-07-11 19:52:59 -0400

    Add project files.

---
+++ Pattern Program.cpp
@@ -0,0 +1,36 @@
+// Pattern Program.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main()
+{
+    int plus;
+    int  rows;
+
+    cout << left << setw(11) << "Pattern A" << left << setw(11) << "Pattern B" << endl;
+
+    for (rows = 0; rows < 10; rows++)
+    {
+
+        for (plus = 0; plus < rows; plus++)
+        {
+            cout << left << "+";
+        }
+
+        cout << setw(11);
+
+        for (plus = 11; plus > rows; plus--)
+        {
+            cout << left << "+";
+        }
+
+        
+        cout << endl;
+        
+    }
+}
+
+
